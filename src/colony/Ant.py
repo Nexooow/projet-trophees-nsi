bdc105c52f9d96a2749553da3fbb4db8e7563885
petertenfields-ui <peter.tenfields@gmail.com>
2026-02-08 16:33:51 +0100

    Debut de sys de move pour fourmis dans anthill

--- src/colony/Ant.py
+++ src/colony/Ant.py
@@ -1,4 +1,6 @@
 import pygame
+from Path_search import a_star_search,converting_to_grid
+import math
 IMAGES={
     "worker":[pygame.image.load(''),[]],
     "nurse":[pygame.image.load(""),[]],
@@ -7,7 +9,7 @@ IMAGES={
     "explorer":[pygame.image.load(''),[]]
 }
 class Ant:
-    def __init__(self,id,power,level,xp,pos_x,pos_yflip=False):
+    def __init__(self,id,power,level,xp,pos_x,pos_y,flip=False):
         self.colony=None
         self.power=power
         self.level=level
@@ -22,8 +24,9 @@ class Ant:
         self.static=self.animation_list[0][1]
         self.flip=flip
         self.angle=0
-        self.rect=pygame.Rect((x,y,self.size*self.image_scale,self.size*self.image_scale))
-        self.img_pos=(self.)
+        self.rect=pygame.Rect((pos_x,pos_y,self.size*self.image_scale,self.size*self.image_scale))
+        self.img_pos=(self.rect.x,self.rect.y )
+        self.speed=1.0
     def update(self):
         self.image = self.animation_list[self.action][self.frame_index]
         animation_cooldown=50
@@ -32,7 +35,7 @@ class Ant:
             self.update_time = pygame.time.get_ticks()
         if self.frame_index >= len(self.animation_list[self.action]):
             self.frame_index=0
-    def draw(self):
+    def draw(self,surface):
         img = pygame.transform.flip(self.image, self.flip, False)
         self.img_pos = (self.rect.x - (self.image_scale),
                         self.rect.y - (self.image_scale))
@@ -40,7 +43,7 @@ class Ant:
         self.mask = pygame.mask.from_surface(img)
     def load_frames(self):
         frames=[]
-        spritesheet,animation_steps=*IMAGES[self.id]
+        spritesheet,animation_steps=IMAGES[self.id][0],IMAGES[self.id][1]
         for y,x in enumerate(animation_steps):
             temp=[]
             for i in range(x):
@@ -49,8 +52,23 @@ class Ant:
                 temp+=[temp_img]
             frames+=[temp]
         return frames
-    def move(self,destination):
-        pass
-    
-
+    def move(self,surface,destination):
+        grid_posx,grid_posy=pixel_to_grid(self.pos_x,self.pos_y)
+        destination_grid=pixel_to_grid(destination[0],destination[1])
+        next=a_star_search(converting_to_grid(surface),(grid_posx,grid_posy),destination_grid)[1]
+        next_pixelx,next_pixely=grid_to_pixel(next[0],next[1])
+        dx,dy=next_pixelx - self.pos_x,next_pixely-self.pos_y
+        dist=math.hypot(dx,dy)
+        if dist<self.speed:
+            self.pos_x,self.pos_y=next_pixelx,next_pixely
+            return
+        dir_x,dir_y=dx/dist,dy/dist
+        self.pos_x+=dir_x*self.speed
+        self.pos_y+=dir_y*self.speed
+        self.orientation=math.degrees(math.atan2(-dir_y,dir_x))
+CELL_SIZE=8
+def pixel_to_grid(px,py):
+    return px//CELL_SIZE,py//CELL_SIZE
+def grid_to_pixel(gx,gy):
+    return gx*CELL_SIZE+CELL_SIZE//2,gy*CELL_SIZE+CELL_SIZE//2
 
