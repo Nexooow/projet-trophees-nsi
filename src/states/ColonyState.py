0122518485935cb055f64e05b704fda86c4a80c8
Nexooow <nschleicher@montaigne-paris.fr>
2026-02-10 17:05:16 +0100

    camera system

--- src/states/ColonyState.py
+++ src/states/ColonyState.py
@@ -10,8 +10,13 @@ class ColonyState (State):
     def __init__ (self, state_manager):
         super().__init__(state_manager, "colony", [])
         self.tasks = TaskManager(self)
+        self.width = 2048
+        self.height = 1152
+        self.world = pygame.Surface((self.width, self.height), pygame.SRCALPHA | pygame.HWSURFACE)
+        self.camera_x = 0
+        self.camera_y = 0
 
-        self.grid = Grid(self.game.height * 0.15, self.game.screen.get_size())
+        self.grid = Grid(self.height*0.15, self.world.get_size())
         self.rooms = []
 
         self.ants = []
@@ -26,24 +31,33 @@ class ColonyState (State):
         for i in range(len(bitmap)):
             for j in range(len(bitmap[i])):
                 if bitmap[i][j]:
-                    pygame.draw.rect(self.game.screen, (82, 40, 23), (x + j, y + i, 1, 1))
+                    pygame.draw.rect(self.world, (82, 40, 23), (x + j, y + i, 1, 1))
 
     def draw_grid(self):
-        start_height = self.game.height * 0.15
+        start_height = self.height * 0.15
         for x, y, cell in self.grid:
             if cell["state"] == "full":
-                pygame.draw.rect(self.game.screen, (82, 40, 23), (x * self.grid.cell_size, start_height + y * self.grid.cell_size, self.grid.cell_size, self.grid.cell_size))
+                pygame.draw.rect(self.world, (82, 40, 23), (x * self.grid.cell_size, start_height + y * self.grid.cell_size, self.grid.cell_size, self.grid.cell_size))
             elif cell["state"] == "partial":
-                pygame.draw.rect(self.game.screen, (150, 0, 0), (x * self.grid.cell_size, start_height + y * self.grid.cell_size, self.grid.cell_size, self.grid.cell_size))
+                pygame.draw.rect(self.world, (150, 0, 0), (x * self.grid.cell_size, start_height + y * self.grid.cell_size, self.grid.cell_size, self.grid.cell_size))
                 #self.draw_bitmap(cell["bitmap"], x, y)
 
     def update (self, events):
-        pass
+        keys = pygame.key.get_pressed()
+        if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and self.camera_x > 0:
+            self.camera_x = min(self.width, self.camera_x-10)
+        elif keys[pygame.K_LEFT] or keys[pygame.K_q]:
+            self.camera_x = max(0, self.camera_x+10)
+        if keys[pygame.K_UP] or keys[pygame.K_z]:
+            self.camera_y = max(0, self.camera_y+10)
+        elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            self.camera_y = min(self.height, self.camera_y-10)
                 
     def draw (self):
-        pygame.draw.rect(self.game.screen, (66, 179, 213), (0, 0, self.game.width, self.game.height*0.12))
+        pygame.draw.rect(self.world, (66, 179, 213), (0, 0, self.width, self.height*0.12))
         # TODO: entre 0.12 et 0.15, faire l'herbe et la végétation pour la transition entre extérieur et la partie
         #  terreuse de la colonie
-        pygame.draw.rect(self.game.screen, (64, 125, 42), (0, self.game.height*0.12, self.game.width, self.game.height*0.04))
-        pygame.draw.rect(self.game.screen, (66, 31, 17), (0, self.game.height*0.15, self.game.width, self.game.height*0.85))
+        pygame.draw.rect(self.world, (64, 125, 42), (0, self.height*0.12, self.width, self.height*0.04))
+        pygame.draw.rect(self.world, (66, 31, 17), (0, self.height*0.15, self.width, self.height*0.85))
         self.draw_grid()
+        self.game.screen.blit(self.world, (self.camera_x, self.camera_y))
