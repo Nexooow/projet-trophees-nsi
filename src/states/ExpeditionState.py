bf9b1b5a7bb45f76636931172d793777307716ca
petertenfields-ui <peter.tenfields@gmail.com>
2026-02-12 22:59:48 +0100

    small ExpeditionState refactor

--- src/states/ExpeditionState.py
+++ src/states/ExpeditionState.py
@@ -15,19 +15,20 @@ class ExpeditionState(State):
         self.screen=pygame.display.set_mode((1200,800))
         self.clock=pygame.time.Clock()
         self.state='map'
-    def run(self):
-        # C juste passager, faudra faire en sorte de separer 
-        running=True
-        while running:
-            if self.state=="map":
-                running=self.map_state()
-            if self.state=="battle":
-                running=self.battle_state()
-        pygame.quit()
+    def update(self,events):
+        if self.state=="map":
+            self.map_state(events)
+        elif self.state=="battle":
+            self.battle_state(events)
+    def draw(self):
+        if self.state=="map":
+            self.draw_map_state()
+        elif self.state=="battle":
+            self.draw_battle_state()
     def map_state(self):
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
-                return False
+                self.stateManager.game.running=False
             if event.type==pygame.MOUSEBUTTONDOWN:
                 clicked_node=self.expedition_map.get_node_at_pos(event.pos[0],event.pos[1])
                 if clicked_node:
@@ -53,13 +54,21 @@ class ExpeditionState(State):
                     self.cam_x-=50
                 if event.type==pygame.K_RIGHT:
                     self.cam_x+=50
+    def draw_map_state(self):
         self.screen.fill((20,20,30)) # Faut trouver une image de bg, ptt un truc qui se genere aussi au fur et a mesure qu'on avance
         pygame.draw.circle(self.screen, (255, 100, 100), (600, 400), 5)
         self.expedition_map.draw(self.screen,self.cam_x,self.cam_y)
         pygame.display.flip()
-        self.clock.tick(60)
-        return True
-    def battle_state(self):
-        current_node=self.expedition_map.current
-        grid=current_node.create_game()
+        
+    def battle_state(self,events):
+        for event in events:
+            if event.type==pygame.QUIT:
+                self.stateManager.game.running=False
+            
+            current_node=self.expedition_map.current
+            grid=current_node.create_game()
+            if event.type==pygame.KEYDOWN:
+                self.state="map"
+    def draw_battle_state(self):
+        pass
 
