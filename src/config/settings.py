404c8a871cd380d80ea1ea8093cdaa6c5637bf84
Nexooow <contact.nekocaffe@gmail.com>
2026-02-12 07:46:48 +0100

    fix: deplacement fichiers configuration pour permettre leur importation

---
+++ src/config/settings.py
@@ -0,0 +1,9 @@
+"""
+Fichier contenant la configuration et les constantes globales du jeu.
+"""
+
+game_name = "Anthill" # TODO: définir le nom
+
+colony_width = 2048
+colony_height = 1152
+colony_underground_start = colony_height * 0.15
