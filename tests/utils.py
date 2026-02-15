8dd464844d5edcd89be11b8b410fbb0f53912eb4
Nikolaï SCHLEICHER <nikolai.schleicher@gmail.com>
2026-02-15 21:17:08 +0100

    Setup test environment in utils.py

--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,11 @@
 """
 Tests pour les classes / fonctions utilitaires.
 """
+
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
 from src.utils.grid import Grid
 
 class TestUtils:
