4d610029008d86754c5795a7fd72a6301aa4b7a3
Nexooow <contact.nekocaffe@gmail.com>
2026-02-15 22:40:31 +0100

    fix: github workflow and added tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,23 +2,24 @@
 Tests pour les classes / fonctions utilitaires.
 """
 
-import sys
-from pathlib import Path
-sys.path.insert(0, str(Path(__file__).parent.parent))
-
 from src.utils.grid import Grid
 
 class TestUtils:
     
     def test_grid(self):
-        grid = Grid((8*5, 8*5))
+        grid = Grid((40, 40))
+        assert grid.width == 5
+        assert grid.height == 5
         assert len(grid.grid) == 5 and len(grid.grid[0]) == 5
         for x in range(grid.width):
             for y in range(grid.height):
                 assert grid.grid[y][x]["state"] == "full"
                 if x == y:
                     grid.set_cell_state(x, y, "empty")
+        
+        # Test A*
         path_possible = grid.a_star((0, 0), (4, 4))
         assert path_possible == [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4)]
+        
         path_impossible = grid.a_star((0, 4), (4, 0))
         assert path_impossible is None
