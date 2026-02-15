dafb6c8a5ea3ba88e9444eb19ae9cff4e01ae7e8
Nexooow <contact.nekocaffe@gmail.com>
2026-02-15 20:39:07 +0100

    refactor: Removes unused files and configuration edit

--- src/config/settings.py
+++ src/config/settings.py
@@ -4,9 +4,12 @@ Fichier contenant la configuration et les constantes globales du jeu.
 
 game_name = "Anthill" # TODO: définir le nom
 
+dirt_color = (168, 88, 72)
+dark_dirt_color = "#542323"
+
 colony_width = 2048
 colony_height = 1152
-colony_underground_start = colony_height * 0.15
+colony_underground_start = 172
 
 colony_brush_size = 20
-colony_brush_color = (255, 105, 97)
+colony_brush_color = "#b12935"
--- src/utils/perlin.py
+++
@@ -1,8 +0,0 @@
-import math
-import random
-
-class PerlinNoise:
-    def __init__ (self, seed=None):
-        self.seed = seed if seed is not None else random.randint(0, 999999)
-        # TODO: implémenter le perlin noise, avec système de seed pour avoir un perlin noise différent à chaque partie,
-        #  mais le même à chaque fois pour une même seed
